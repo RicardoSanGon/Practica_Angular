d6ff5ff9a5d074228aeb236c478aa5bc76aa94aa
AngelGa3L <angelgaelguevara@gmail.com>
2024-03-11 14:19:06 -0600

    Correo orden

---
+++ ComisionQA/app/Mail/OrdenAceptadaMail.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class OrdenAceptadaMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $orderDetail;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($orderDetail)
+    {
+        $this->orderDetail = $orderDetail;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function build()
+    {
+        return $this->subject('Orden Aceptada Mail')
+                    ->view('emails.orden-aceptada');
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}

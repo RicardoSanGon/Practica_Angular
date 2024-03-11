9dff2845859b0f554998f139989b951235249e07
RicardoSanGon <22170038@uttcampus.edu.mx>
2024-03-11 13:15:02 -0600

    Angular

---
+++ PracticaEquipo/src/app/shared/reg-modelo/reg-modelo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reg-modelo',
+  standalone: true,
+  imports: [],
+  templateUrl: './reg-modelo.component.html',
+  styleUrl: './reg-modelo.component.css'
+})
+export class RegModeloComponent {
+
+}
---
+++ PracticaEquipo/src/app/shared/reg-proveedor/reg-proveedor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reg-proveedor',
+  standalone: true,
+  imports: [],
+  templateUrl: './reg-proveedor.component.html',
+  styleUrl: './reg-proveedor.component.css'
+})
+export class RegProveedorComponent {
+
+}

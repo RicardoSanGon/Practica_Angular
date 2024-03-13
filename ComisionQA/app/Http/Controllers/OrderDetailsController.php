4b0905b74fe87e15ed1ff7b972e50d8672d13d37
AngelGa3L <angelgaelguevara@gmail.com>
2024-03-13 11:23:05 -0600

    Order details index

--- ComisionQA/app/Http/Controllers/OrderDetailsController.php
+++ ComisionQA/app/Http/Controllers/OrderDetailsController.php
@@ -15,50 +15,50 @@ use App\Mail\OrdenAceptadaMail;
 
 class OrderDetailsController extends Controller
 {
-    public function index(Request $request)
-    {
-        $users = User::find(UsersController::getUserIdFromToken($request->header('authorization')));
-        if ($users->role_id == 1) {
-            $order_details = Order_Detail::all();
-            $order_details = $order_details->map(function ($order_detail) {
-                return [
-                    "id" => $order_detail->id,
-                    "cantidad" => $order_detail->quantity,
-                    "modelo" => $order_detail->vehicle_model->model_name,
-                    "orden_id" => $order_detail->order_id,
-                    "precio_total" => $order_detail->price,
-                    "status" => $order_detail->status,
-                    "fecha_de_entrega" => $order_detail->delery_date
-                ];
-            });
-            return response()->json(['data' => $order_details], 200);
-        }
-
-        if ($users->role_id == 2) {
-            $customer = Customer::where('user_id', UsersController::getUserIdFromToken($request->header('authorization')))->first();
-            if (!$customer) {
-                return response()->json(["msg" => "El usuario no esta registrado como cliente"], 400);
-            }
-            $order = Order::where('customer_id', $customer->id)->latest('order_date')->first();
-            if (!$order) {
-                return response()->json(["msg" => "El usuario no tiene ordenes"], 400);
-            }
-            $order_details = Order_Detail::where('order_id', $order->id)->get();
-            $order_details = $order_details->map(function ($order_detail) {
-                return [
-                    "id" => $order_detail->id,
-                    "cantidad" => $order_detail->quantity,
-                    "modelo" => $order_detail->vehicle_model->model_name,
-                    "orden_id" => $order_detail->order_id,
-                    "precio_total" => $order_detail->price,
-                    "status" => $order_detail->status,
-                    "fecha_de_entrega" => $order_detail->delery_date
-                ];
-            });
-            return response()->json(['data' => $order_details], 200);
-        }
-        return response()->json(["msg" => "No tiene permisos"], 401);
+    public function index(Request $request, $id)
+{
+    $users = User::find(UsersController::getUserIdFromToken($request->header('authorization')));
+    if ($users->role_id == 1) {
+        $order_details = Order_Detail::where('id', $id)->get();
+        $order_details = $order_details->map(function ($order_detail) {
+            return [
+                "id" => $order_detail->id,
+                "cantidad" => $order_detail->quantity,
+                "modelo" => $order_detail->vehicle_model->model_name,
+                "orden_id" => $order_detail->order_id,
+                "precio_total" => $order_detail->price,
+                "status" => $order_detail->status,
+                "fecha_de_entrega" => $order_detail->delery_date
+            ];
+        });
+        return response()->json(['data' => $order_details], 200);
     }
+
+    if ($users->role_id == 2) {
+        $customer = Customer::where('user_id', UsersController::getUserIdFromToken($request->header('authorization')))->first();
+        if (!$customer) {
+            return response()->json(["msg" => "El usuario no esta registrado como cliente"], 400);
+        }
+        $order = Order::where('customer_id', $customer->id)->latest('order_date')->first();
+        if (!$order) {
+            return response()->json(["msg" => "El usuario no tiene ordenes"], 400);
+        }
+        $order_details = Order_Detail::where('order_id', $order->id)->where('id', $id)->get();
+        $order_details = $order_details->map(function ($order_detail) {
+            return [
+                "id" => $order_detail->id,
+                "cantidad" => $order_detail->quantity,
+                "modelo" => $order_detail->vehicle_model->model_name,
+                "orden_id" => $order_detail->order_id,
+                "precio_total" => $order_detail->price,
+                "status" => $order_detail->status,
+                "fecha_de_entrega" => $order_detail->delery_date
+            ];
+        });
+        return response()->json(['data' => $order_details], 200);
+    }
+    return response()->json(["msg" => "No tiene permisos"], 401);
+}
 
     public function store(Request $request)
     {
